9a212a5722e08ef720af0cbc78fffa0f03608779
Bhola-Paul <paulbhola1987@gmail.com>
2025-12-06 18:49:09 +0530

    Resolved merge conflicts

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -15,6 +14,7 @@ import {
 } from "./pages/index.js";
 import store from "./store/store.js";
 import Navbar from "./components/Navbar.jsx";
+import ChatBot from "./pages/ChatBot.jsx";
 
 const router = createBrowserRouter([
   {
@@ -47,56 +47,21 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path:"/advisory",
-        element:<CropAdvisory/>
-      }
+        path: "/advisory",
+        element: <CropAdvisory />,
+      },
+      {
+        path: "/chatbot",
+        element: <ChatBot />,
+      },
     ],
   },
 ]);
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-// import store from './store/store.js';
-import { FarmerRegistration, Home } from './pages/index.js';
-import store from './store/store.js';
-import ChatBot from './pages/ChatBot.jsx';
-
-const router = createBrowserRouter([{
-  path: '/',
-  element: <App />,
-  children: [
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: '/farmer-registration',
-      element: <FarmerRegistration />
-    },
-    {
-      path: '/chatBot',
-      element: <ChatBot />
-    },
-  ]
-}])
->>>>>>> Stashed changes
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< Updated upstream
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
-=======
-  <Provider store={store}>
-      <RouterProvider router={router} />
-  </Provider>
-  </StrictMode>,
-)
->>>>>>> Stashed changes

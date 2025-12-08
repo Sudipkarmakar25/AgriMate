f327bf3a6b58a1007c09a5c20ecbf287859fab52
Sudipkarmakar25 <sudipk166@gmail.com>
2025-12-08 13:27:10 +0530

    created cdisease remedy controler

--- Backend/routes/MessageRoutes.js
+++ Backend/routes/MessageRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { textMessageController } from '../controllers/Message Controller/TextMessageController.js';
 import { imageMessageController } from '../controllers/Message Controller/ImageMessageController.js';
-
+import {DiseaseRemedyController} from '../controllers/Message Controller/DiseaseRemedyController.js'
 const messageRouter=express.Router();
 
 messageRouter.post("/text", (req, res, next) => {
@@ -11,5 +11,5 @@ messageRouter.post("/text", (req, res, next) => {
 }, authMiddleware, textMessageController);
 
 messageRouter.post('/image',authMiddleware,imageMessageController);
-
+messageRouter.post('/remedy',authMiddleware,DiseaseRemedyController);
 export default messageRouter;
